a9d3d5b53d2026ed3eb78edad21ff1e4c0bdabf8
Ian Hancock <northroadian@gmail.com>
2020-12-31 14:55:14 -0600

    finished simple testing app

--- test/apptest.js
+++ test/apptest.js
@@ -1,13 +1,32 @@
 
 const assert = require('chai').assert;
-const sayHello = require ('../app').sayHello; 
+//const sayHello = require ('../app').sayHello; 
+//const addNumbers = require ('../app').addNumbers; 
+const app = require('../app');
 
-//const app = require('../app');
+sayHelloResult = app.sayHello();
+addNumbersResult  = app.addNumbers(5,5); 
 
-describe ('App',function (){
-    it('app should return hello', function(){
-        let result = sayHello();
+describe ('App', function (){
+    it('sayHello should return hello', function(){
+        let result = app.sayHello();
         assert.equal(result, 'hello'); 
-
     });
-})
+
+
+    it ('sayHello should return type string', function(){
+        let result = app.sayHello();
+        assert.typeOf(result,'string')
+    });
+
+    it('addNumbers should be above 5', function (){
+        
+        let result= app.addNumbers(5,5);
+        assert.isAbove(result, 5)
+    })
+    it ('assNumbers should return type number', function(){
+        let result = app.addNumbers(5,5);
+        assert.typeOf(result, 'number')
+    });
+
+});
